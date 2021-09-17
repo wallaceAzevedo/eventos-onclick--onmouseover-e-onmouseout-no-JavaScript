db0e53ee00f5202080440f4a69deb7050b7aaf12
Emma Paris <eparis@atlassian.com>
2021-09-16 21:14:59 -0300

    .

---
+++ personalizado.js
@@ -0,0 +1,11 @@
+function carConteudo(){
+    document.getElementById("conteudo").innerHTML = "Integer ac pulvinar risus, vel lobortis sapien. Maecenas ut iaculis velit. Fusce diam augue, sodales et purus sed, pharetra rutrum erat.";
+}
+
+function mouseOver(){
+    document.getElementById("mouseAlt").innerHTML = "Retire o mouse";
+}
+
+function mouseOut(){
+    document.getElementById("mouseAlt").innerHTML = "Passe o mouse";
+}
